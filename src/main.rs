b4acacb67d8133f03599f7d922ee36f62118a143
Thomas Bivins <bivinsthomas@gmail.com>
2022-01-04 15:13:05 -0500

    Variables and types

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-mod print;
+//mod print;
+//mod vars;
+mod types;
 
 //CREATING NEW PROJECT: cargo new --bin project_name
 //RUNNING: cargo run
 
 fn main() {
-    print::run();
+    //print::run();
+    //vars::run();
+    types::run();
 }
